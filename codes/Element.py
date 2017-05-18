55bd502d07a4a0fd9a8c4bde71f106d9b22716b4
yuchao <harry.t.chao@gmail.com>
2017-05-18 15:20:29 +0800

    print Element works

--- codes/Element.py
+++ codes/Element.py
@@ -5,15 +5,27 @@ def enum(**enums):
 
 class Element:
 
-  def __init__(self, name, children, parent=None, level=None, element_type=None, wv=None):
+  def __init__(self, name='', children=[], parent=None, level=None, element_type=None, wv=None):
     self.name = name
     self.children = children
     self.parent = parent
     self.level = level
     self.element_type = element_type
     self.wv = wv
-    def __str__(self):
-      return "{{'name':{0}, 'parent':{1}, 'children':[{2}]}}".format(self.name, self.parent, ','.join(map(lambda x:x.__str__(), self.children)))
+
+  def _str_(self, prefix, e):
+    s = ''
+    prefix += ('--' if e.element_type == Element.ElementType.relation else '-->') + e.name
+    if e.children:
+      for sub_e in e.children:
+        s += self._str_(prefix, sub_e)
+    else:
+      s = prefix+'\n'
+    return s
+    #return "{{'name':{0}, 'children':[{1}]}}".format(self.name, ','.join(map(lambda x:x._str_(), self.children)))
+
+  def __str__(self):
+    return self._str_('', self) 
 
   ElementType = enum(entity='entity', relation='relation')
 

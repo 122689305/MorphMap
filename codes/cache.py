ee04c2ab1d97fa101137baf2f0997600e40e0c25
yuchao <harry.t.chao@gmail.com>
2017-05-22 18:09:05 +0800

    separate cache function outside as a independent function

---
+++ codes/cache.py
@@ -0,0 +1,12 @@
+# cache
+def cache(filename, func, *args, **keywords):
+	import os
+	existed = os.path.isfile(filename)
+	if existed:
+		cached = open(filename, 'rb')
+		return pickle.load(cached)
+	else:
+		data = func(*args, **keywords)
+		pickle.dump(data, open(filename, 'wb'))
+		return data
+
